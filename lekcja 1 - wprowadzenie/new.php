7043583513d6ce76268cf2c6c25ada37b3aca76c
Christian Sadecki <christiansadecki1@gmail.com>
2022-09-15 09:59:25 +0200

    Updated readme, wprowadzenie - zadanie.php

---
+++ lekcja 1 - wprowadzenie/new.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<meta charset="utf-8">
+
+
+
+
+
+
+<title>
+Strona
+</title>
+
+
+
+
+
+
+</head>
+
+
+
+
+
+<body>
+<center>
+Hello World
+</center>
+<?php
+    $zmienna1 = "My counter 1";
+    echo "Hello world", "<br>". $zmienna1. "<br>", "Hello friend";
+    
+?>
+</body>
+</html>
