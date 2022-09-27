354f4643f67b60f4166fd9ea714e396f5425d16d
Christian Sadecki <christiansadecki1@gmail.com>
2022-09-27 10:10:08 +0200

    Updated, readme, dodany plik tablice

---
+++ lekcja 3 - tablice/zadanie-tablice.php
@@ -0,0 +1,12 @@
+ <?php
+            $paper[] = "Hobbit";
+            $paper[] = "Chłopcy z placu broni";
+            $paper[] = "Antygona";
+            $paper[] = "Dziady cz.IV";
+
+         
+
+            for($j =0; $j < 4; ++$j){
+                echo "$paper[$j] <br>";
+            }
+        ?>
